e144d0b1e890e5c2be48484cb6c9c47ef3e6ec32
Bandara K. M. E <117734215+Isuru936@users.noreply.github.com>
2024-07-01 01:44:50 +0530

    configs

--- client/src/pages/Issue Management/AddIssue.jsx
+++ client/src/pages/Issue Management/AddIssue.jsx
@@ -82,87 +82,100 @@ function AddIssue() {
   return (
     <div className="w-screen h-full bg-gray-800 pb-5">
       <NavBar email={currentUser.email} />
-      <div className="w-screen h-full max-w-md mt-5 mb-5 mx-auto bg-gray-700 shadow-md rounded-lg p-10">
+      <div className="w-full h-full max-w-md lg:max-w-none mt-5 mb-5 mx-auto bg-gray-700 shadow-md rounded-lg p-10">
         <h2 className="text-3xl text-gray-200 border-b-2 border-gray-800 font-bold mb-5">
           Create a New Issue
         </h2>
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-50">
-              Assignee Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={issue.email}
-              onChange={handleChange}
-              className="w-full mt-1 p-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-50">Subject</label>
-            <input
-              type="text"
-              name="subject"
-              value={issue.subject}
-              onChange={handleChange}
-              className="w-full mt-1 p-2 border rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-50">Description</label>
-            <textarea
-              name="description"
-              value={issue.description}
-              onChange={handleChange}
-              className="w-full mt-1 p-2 border rounded"
-              required
-            ></textarea>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-50">Technician</label>
-            <select
-              name="technician"
-              value={issue.technician}
-              onChange={handleChange}
-              className="w-full mt-1 p-2 border rounded"
-            >
-              <option value="">Select Technician</option>
-              {technicians.map((tech, index) => (
-                <option key={index} value={tech.name}>
-                  {tech.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Image</label>
-            <input
-              type="file"
-              onChange={handleFileUpload}
-              accept="image/*"
-              className="w-full mt-1 p-2 border rounded"
-            />
-            <img
-              src={
-                issue.imageURL
-                  ? issue.imageURL
-                  : "https://via.placeholder.com/150"
-              }
-              alt=" Preview"
-              className="mt-2 w-full h-40 object-cover rounded"
-            />
-          </div>
-          <div>{/* image upload */}</div>
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-            >
-              Add Issue
-            </button>
+          <div className=" block gap-0 lg:flex lg:gap-10">
+            <div className="flex-1">
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-50">
+                  Assignee Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={issue.email}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border rounded"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-50">Subject</label>
+                <input
+                  type="text"
+                  name="subject"
+                  value={issue.subject}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border rounded"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-50">Description</label>
+                <textarea
+                  name="description"
+                  value={issue.description}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border rounded"
+                  required
+                ></textarea>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-50">Technician</label>
+                <select
+                  name="technician"
+                  value={issue.technician}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border rounded"
+                >
+                  <option value="">Select Technician</option>
+                  {technicians.map((tech, index) => (
+                    <option key={index} value={tech.name}>
+                      {tech.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="flex-1">
+              <div className="mb-4">
+                <label className="block text-gray-700">Image</label>
+                <input
+                  type="file"
+                  onChange={handleFileUpload}
+                  accept="image/*"
+                  className="w-full mt-1 p-2 border rounded"
+                />
+                <img
+                  src={
+                    issue.imageURL
+                      ? issue.imageURL
+                      : "https://via.placeholder.com/150"
+                  }
+                  alt=" Preview"
+                  className="mt-2 w-full h-64 object-contain  rounded"
+                />
+              </div>
+              <div>{/* image upload */}</div>
+              <div className="flex gap-5 justify-end">
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                >
+                  Add Issue
+                </button>
+                <button
+                  type="button"
+                  onClick={() => navigate("/admin")}
+                  className="bg-blue-800 text-white px-4 py-2 rounded hover:bg-blue-900"
+                >
+                  Back
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </div>
