350a8ffc813c86e46694ce07f224cfb43f63a194
Bandara K. M. E <117734215+Isuru936@users.noreply.github.com>
2024-07-02 13:59:02 +0530

    configured deploy settings CORS

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
         target: "https://ingenious-freedom-production-8978.up.railway.app/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
       },
     },
